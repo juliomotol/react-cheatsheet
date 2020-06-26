2574c30b5e0c83ce1c245ea3ada0ad5e10e05396
juliomotol <juliomotol@users.noreply.github.com>
2020-06-26 15:07:47 +0000

    deploy: fe995c9a82cf970e209d5c4f7f77cd2f8065ffbc

---
+++ precache-manifest.4aeae11464926f6d9a1f89b72a8744bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc5fa9853e1b517907e152b129cdf52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "040c6f6db7a77da74705",
+    "url": "/static/css/main.c10e5b57.chunk.css"
+  },
+  {
+    "revision": "01e69b4374ff6e6f382a",
+    "url": "/static/js/2.d8113282.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d8113282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040c6f6db7a77da74705",
+    "url": "/static/js/main.61bf37f3.chunk.js"
+  },
+  {
+    "revision": "50ce5e0bfb137ab2898e",
+    "url": "/static/js/runtime-main.6429fc70.js"
+  }
+]);
