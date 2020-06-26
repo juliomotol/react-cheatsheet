a369331a004cedf8825827f1e38c7c7a1bdce3b3
juliomotol <juliomotol@users.noreply.github.com>
2020-06-26 16:03:29 +0000

    deploy: 469e5f67cada5413dc106f42e7cb15cc14d6f9d5

---
+++ precache-manifest.38f4c7f61e76729a9c34e6782a214e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15065700362cff2fcf3ac08c13160d3",
+    "url": "/react-cheatsheet/index.html"
+  },
+  {
+    "revision": "1ccdebe105b39e90b97a",
+    "url": "/react-cheatsheet/static/css/main.c10e5b57.chunk.css"
+  },
+  {
+    "revision": "01e69b4374ff6e6f382a",
+    "url": "/react-cheatsheet/static/js/2.d8113282.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-cheatsheet/static/js/2.d8113282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccdebe105b39e90b97a",
+    "url": "/react-cheatsheet/static/js/main.303fd0c8.chunk.js"
+  },
+  {
+    "revision": "702560cec89b9cdcafa8",
+    "url": "/react-cheatsheet/static/js/runtime-main.af4c8ecd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4aeae11464926f6d9a1f89b72a8744bf.js"
+  "/react-cheatsheet/precache-manifest.38f4c7f61e76729a9c34e6782a214e9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cheatsheet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
